395750cfba67dac444dd720e9360e3a35512d3dd
unknown <sarojsardar25@gmail.com>
2024-05-29 08:53:14 +0545

    medical status enum

---
+++ app/Enum/UserMedicalStatus.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Enum;
+use App\Abstracts\Enum;
+
+class UserMedicalStatus extends Enum{
+
+    const New = 'New';
+
+    const Fit = 'Fit';
+
+    const Unfit = 'Unfit';
+
+    const Kiv = 'Kiv';
+
+}
